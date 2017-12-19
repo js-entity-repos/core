a6dab3a027a4e8bca1cc8e5d7bd6c2f7b1efcc27
Ryan Smith <0ryansmith1994@gmail.com>
2017-12-19 14:11:05 +0000

    feat(patchEntity): Adds tests for patchEntity.

--- src/tests/overwriteEntity/test.ts
+++ src/tests/overwriteEntity/test.ts
@@ -6,24 +6,24 @@ import Facade from '../../Facade';
 import { TestEntity, testEntity, testId, TestId } from '../testEntity';
 
 export default (facade: Facade<TestId, TestEntity>) => {
-  describe('removeEntity', () => {
+  describe('overwriteEntity', () => {
     it('should error when identifier does not exist', async () => {
       const promise = facade.overwriteEntity({ id: testId, entity: testEntity });
       await assertRejects(promise, MissingEntityError);
     });
 
     it('should overwrite when identifier does exist', async () => {
-      const overwrite: TestEntity = {
+      const testOverwrite: TestEntity = {
         ...testId,
         booleanProp: false,
         numberProp: 2,
         stringProp: 'test_string_prop_overwrite',
       };
       await facade.createEntity({ entity: testEntity });
-      const overwrittenEntity = await facade.overwriteEntity({ id: testId, entity: overwrite });
+      const overwrittenEntity = await facade.overwriteEntity({ id: testId, entity: testOverwrite });
       const retrievedEntity = await facade.getEntity({ id: testId });
-      assert.deepEqual(overwrittenEntity, overwrite);
-      assert.deepEqual(retrievedEntity, overwrite);
+      assert.deepEqual(overwrittenEntity, testOverwrite);
+      assert.deepEqual(retrievedEntity, testOverwrite);
     });
   });
 };
---
+++ src/tests/patchEntity/test.ts
@@ -0,0 +1,41 @@
+import * as assertRejects from 'assert-rejects';
+import 'mocha'; // tslint:disable-line:no-import-side-effect
+import * as assert from 'power-assert';
+import MissingEntityError from '../../errors/MissingEntityError';
+import Facade from '../../Facade';
+import { TestEntity, testEntity, testId, TestId } from '../testEntity';
+
+export default (facade: Facade<TestId, TestEntity>) => {
+  describe('patchEntity', () => {
+    const assertPatch = async (testPatch: Partial<TestEntity>) => {
+      const patchedEntity = await facade.patchEntity({ id: testId, patch: testPatch });
+      const retrievedEntity = await facade.getEntity({ id: testId });
+      const expectedEntity = { ...testEntity, ...testPatch };
+      assert.deepEqual(patchedEntity, expectedEntity);
+      assert.deepEqual(retrievedEntity, expectedEntity);
+    };
+
+    it('should error when identifier does not exist', async () => {
+      const promise = facade.overwriteEntity({ id: testId, entity: testEntity });
+      await assertRejects(promise, MissingEntityError);
+    });
+
+    it('should patch when patching one entity property', async () => {
+      const testPatch: Partial<TestEntity> = {
+        stringProp: 'test_string_prop_patch',
+      };
+      await facade.createEntity({ entity: testEntity });
+      await assertPatch(testPatch);
+    });
+
+    it('should patch when patching all entity properties', async () => {
+      const testPatch: Partial<TestEntity> = {
+        booleanProp: false,
+        numberProp: 2,
+        stringProp: 'test_string_prop_patch',
+      };
+      await facade.createEntity({ entity: testEntity });
+      await assertPatch(testPatch);
+    });
+  });
+};
