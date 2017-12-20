34d5b83e95cbdc34e29fab58a7b27220e780b573
Ryan Smith <0ryansmith1994@gmail.com>
2017-12-20 10:29:52 +0000

    feat(package): Adds facade to export.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import * as errors from './errors';
+import FacadeInterface from './Facade';
 import * as signatures from './signatures';
 import * as tests from './tests';
 import * as types from './types';
 import * as utils from './utils';
+
+export type Facade<Id, Entity> = FacadeInterface<Id, Entity>;
 
 export default {
   errors,
