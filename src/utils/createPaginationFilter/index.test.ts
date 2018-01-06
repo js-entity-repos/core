85cd391817dd1da866180d98a1e7255095cf704d
Ryan Smith <0ryansmith1994@gmail.com>
2018-01-06 12:41:17 +0000

    test(createPaginationFilter): Improves coverage.

--- src/utils/createPaginationFilter/index.test.ts
+++ src/utils/createPaginationFilter/index.test.ts
@@ -1,12 +1,14 @@
 import 'mocha'; // tslint:disable-line:no-import-side-effect
 import * as assert from 'power-assert';
 import { TestEntity, testEntity } from '../../tests/utils/testEntity';
+import { Filter } from '../../types/Filter';
 import Pagination from '../../types/Pagination';
+import Sort from '../../types/Sort';
 import createCursorFromEntity from '../createCursorFromEntity';
 import createPaginationFilter from './index';
 
 describe('createCursorFromEntity', () => {
-  const sort = { id: true };
+  const sort: Sort<TestEntity> = { id: true, booleanProp: false };
 
   it('should return empty filter when the cursor is undefined', () => {
     const pagination: Pagination = { cursor: undefined, forward: true, limit: 1 };
@@ -19,7 +21,8 @@ describe('createCursorFromEntity', () => {
     const cursor = createCursorFromEntity<TestEntity>(testEntity, sort);
     const pagination: Pagination = { cursor, forward: true, limit: 1 };
     const actualResult = createPaginationFilter<TestEntity>(pagination, sort);
-    const expectedResult = {
+    const expectedResult: Filter<TestEntity> = {
+      booleanProp: { $lt: testEntity.booleanProp },
       id: { $gt: testEntity.id },
     };
     assert.deepEqual(actualResult, expectedResult);
@@ -29,7 +32,8 @@ describe('createCursorFromEntity', () => {
     const cursor = createCursorFromEntity<TestEntity>(testEntity, sort);
     const pagination: Pagination = { cursor, forward: false, limit: 1 };
     const actualResult = createPaginationFilter<TestEntity>(pagination, sort);
-    const expectedResult = {
+    const expectedResult: Filter<TestEntity> = {
+      booleanProp: { $gt: testEntity.booleanProp },
       id: { $lt: testEntity.id },
     };
     assert.deepEqual(actualResult, expectedResult);
